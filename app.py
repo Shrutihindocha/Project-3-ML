a830d8b790273dd9a0873d5bc2f98b98efc7d523
P219-C <19741043+P219-C@users.noreply.github.com>
2022-02-28 20:37:43 +0800

    Added Arima/Flask component

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
 import requests
 import json
-# from sqlalchemy import create_engine, inspect
-# import pandas as pd
+import joblib
 
 # flask app setup
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    
     return render_template("index.html")
 
 @app.route("/about")
@@ -27,7 +25,6 @@ def raw():
 
     return raw_data
 
-
 # Route to the clean data
 @app.route("/about/api/data")
 def data():
@@ -35,6 +32,12 @@ def data():
         cleaned_data = json.load(json_file)
     return cleaned_data
 
+
+@app.route("/api/predict/<country>/<num_periods>")
+def prediction(country,num_periods): 
+    model = joblib.load(f"models/{country}.sav")
+    forecast = model.forecast(steps=int(num_periods)).tolist()
+    return {"prediction": forecast}
 
 
 @app.route("/results")
