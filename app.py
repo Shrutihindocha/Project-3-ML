c15e5e0c9108b9de67c46ec181e651dbb0cd204d
shrutihindocha <shruti.hindocha4@outlook.com>
2022-02-28 21:11:26 +0800

    app.py updated

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/about/api/raw")
+@app.route("/about/raw")
 def raw():
     url_HDRO = "http://ec2-54-174-131-205.compute-1.amazonaws.com/API/HDRO_API.php/indicator_id=137906,136906,137006/year=1990,1995,2000,2005,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021"
     response_HDRO = requests.get(url_HDRO)
@@ -26,23 +26,12 @@ def raw():
     return raw_data
 
 # Route to the clean data
-@app.route("/about/api/data")
+@app.route("/about/data")
 def data():
     with open('./Data/data_final.json') as json_file:
         cleaned_data = json.load(json_file)
     return cleaned_data
 
-<<<<<<< HEAD
-=======
-
-@app.route("/api/predict/<country>/<num_periods>")
-def prediction(country,num_periods): 
-    model = joblib.load(f"models/{country}.sav")
-    forecast = model.forecast(steps=int(num_periods)).tolist()
-    return {"prediction": forecast}
-
-
->>>>>>> ec00448a1c2c0eef2857754e3be6d84a53fdeb5e
 @app.route("/results")
 def results():
     return render_template("results.html")
